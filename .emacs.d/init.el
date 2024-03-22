5ca8247d3c372b8e95444232cc543d632aa6d3d5
slouis214 <slouis.214@gmail.com>
2024-03-22 23:07:37 +0900

    First commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,17 @@
+(set-language-environment "Japanese")
+
+;; load-path
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+	      (expand-file-name (concat user-emacs-directory path))))
+	(add-to-list 'load-path default-directory)
+	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+	    (normal-top-level-add-subdirs-to-load-path))))))
+
+(add-to-load-path "elisp" "conf" "public_repos")
+
+;; init-loader
+(require 'init-loader)
+(init-loader-load "~/.emacs.d/conf")
